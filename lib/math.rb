7fe920282f681208b0316be111c330cba5ecb2cc
Peter Park 2.0 <peterparkk25+@gmail.com>
2020-12-27 19:16:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,11 +5,13 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  
+  difference = (num1 - num2)
+  puts difference
+  difference
 end
 
 def division(num1, num2)
-
+  
 end
 
 def multiplication(num1, num2)
